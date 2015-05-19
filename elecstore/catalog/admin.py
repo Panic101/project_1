dadd6dcd1565166ed910c975554b8b10d084cf39
hendrik <hendrikmarais@hotmail.com>
2015-05-19 15:22:39 +0200

    Created a back-end for my catalog webapp - and modified the view a bit :) #YOLO

--- elecstore/catalog/admin.py
+++ elecstore/catalog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from .models import ProductDirectory, ProductItem
 # Register your models here.
+
+class ProductItemInline(admin.TabularInline):
+    model = ProductItem
+    extra = 1
+
+class ProductDirectoryAdmin(admin.ModelAdmin):    
+    fieldsets = [
+        ('Directory Name',               {'fields': ['directory_name'], 'classes': ['collapse']}),
+        ('Total Items in Directory', {'fields': ['total_items'], 'classes': ['collapse']}),
+    ]
+    inlines = [ProductItemInline]
+    list_display = ('directory_name', 'total_items')
+    search_fields = ['directory_name']
+
+admin.site.register(ProductDirectory, ProductDirectoryAdmin)
+
--- elecstore/catalog/models.py
+++ elecstore/catalog/models.py
@@ -6,6 +6,8 @@ class ProductDirectory(models.Model):
     directory_name = models.CharField(max_length=200)
     #total_items = models.DateTimeField('date published')
     total_items = models.IntegerField(default=0)
+    def __str__(self):              # __unicode__ on Python 2
+        return self.directory_name
 
 
 class ProductItem(models.Model):
@@ -13,3 +15,5 @@ class ProductItem(models.Model):
     product_name = models.CharField(max_length=200)
     product_description = models.CharField(max_length=200)
     product_price = models.DecimalField(max_digits=8, decimal_places=2)
+    def __str__(self):              # __unicode__ on Python 2
+        return self.product_name
