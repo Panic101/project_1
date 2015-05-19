2508802d0ad350e73f146e107340c1a6cdfb8397
hendrik <hendrikmarais@hotmail.com>
2015-05-19 12:51:31 +0200

    there was an error in modules - its fixed now: modules are now also officially a reggistered app within the electrostor.settings.py file :)

--- elecstore/catalog/models.py
+++ elecstore/catalog/models.py
@@ -5,11 +5,11 @@ from django.db import models
 class ProductDirectory(models.Model):
     directory_name = models.CharField(max_length=200)
     #total_items = models.DateTimeField('date published')
-    total_items = models.IntegerField()
+    total_items = models.IntegerField(default=0)
 
 
 class ProductItem(models.Model):
     product_directory = models.ForeignKey(ProductDirectory)
     product_name = models.CharField(max_length=200)
     product_description = models.CharField(max_length=200)
-    product_price = models.models.DecimalField(max_digits=8, decimal_places=2)
+    product_price = models.DecimalField(max_digits=8, decimal_places=2)
