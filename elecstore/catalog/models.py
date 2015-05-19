627c72fd5859054796503c9b9eadd64eb85745d9
hendrik <hendrikmarais@hotmail.com>
2015-05-19 12:30:39 +0200

    the catalog webapp now has 2 modules that I created

--- elecstore/catalog/models.py
+++ elecstore/catalog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class ProductDirectory(models.Model):
+    directory_name = models.CharField(max_length=200)
+    #total_items = models.DateTimeField('date published')
+    total_items = models.IntegerField()
+
+
+class ProductItem(models.Model):
+    product_directory = models.ForeignKey(ProductDirectory)
+    product_name = models.CharField(max_length=200)
+    product_description = models.CharField(max_length=200)
+    product_price = models.models.DecimalField(max_digits=8, decimal_places=2)
