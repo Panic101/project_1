926466d5719f223573127b49a327e405ca6e7f72
hendrik <hendrikmarais@hotmail.com>
2015-05-20 07:20:52 +0200

    Now my website have official pages - I added some basic views and linked them up with catalog.urls and urlsconfig(in electrostore folder) :)

--- elecstore/catalog/models.py
+++ elecstore/catalog/models.py
@@ -13,7 +13,7 @@ class ProductDirectory(models.Model):
 class ProductItem(models.Model):
     product_directory = models.ForeignKey(ProductDirectory)
     product_name = models.CharField(max_length=200)
-    product_description = models.CharField(max_length=200)
+    product_description = models.TextField()
     product_price = models.DecimalField(max_digits=8, decimal_places=2)
     def __str__(self):              # __unicode__ on Python 2
         return self.product_name
--- elecstore/catalog/views.py
+++ elecstore/catalog/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def homePage(request):
+	return HttpResponse("This will be my home page, with about us and that crap that can come in later")
+	
+def AbourUsPage(request):
+	return HttpResponse("This will be the official about us page")
+	
+def ContactUsPage(request):
+	return HttpResponse("This will be the official contact us page")
+
+def catalogView(request):
+    return HttpResponse("In this page, the different catalogs will be listed")
+    
+def catalogViewItem(request, directory_id):
+    return HttpResponse("Im not firm on the details yet, but this displays catalog directory%s." % directory_id)
+
+def productView(request):			#product_id
+    response = "Once the user selected a category, they will view the products here"
+    return HttpResponse(response)
+    
+def productViewItem(request, product_id):			
+    return HttpResponse("Again, not so sure how yet, but this will display product%s" % product_id)
+
+
